cbf405b94842ce835902cbd70eba616694dd2f05
Mark Thompson <mark.thompson@smpl.company>
2020-11-13 13:13:26 -0800

    Generate an App: Update app template to be simplier and updated app spec to reflect changed text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -6,11 +7,14 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      schemas: [
+        NO_ERRORS_SCHEMA,
+      ]
     }).compileComponents();
   });
 
@@ -30,6 +34,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('personal-place-my-order app is running!');
+    // expect(compiled.querySelector('.content span').textContent).toContain('personal-place-my-order app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Place My Order App: Coming Soon!');
   });
 });
