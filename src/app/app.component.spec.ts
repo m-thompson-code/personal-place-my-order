6da8806598bcb69a8111a320af6496a1740ea9f3
Mark Thompson <mark.thompson@smpl.company>
2020-11-13 14:53:15 -0800

    added routing for home and restaurants with specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,37 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ImageUrlPipe } from './image-url.pipe';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 describe('AppComponent', () => {
+  let router: Router;
+  let location: Location;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        // RouterTestingModule,
+        AppRoutingModule,
       ],
       declarations: [
-        AppComponent,
+        AppComponent, HomeComponent, RestaurantComponent, ImageUrlPipe
       ],
       schemas: [
         NO_ERRORS_SCHEMA,
       ]
     }).compileComponents();
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
@@ -36,5 +52,21 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     // expect(compiled.querySelector('.content span').textContent).toContain('personal-place-my-order app is running!');
     expect(compiled.querySelector('h1').textContent).toContain('Place My Order App: Coming Soon!');
+  });
+
+  it('should render the HomeComponent with router navigates to "/" path', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    router.navigate(['']).then(() => {
+      expect(location.path()).toBe('');
+      expect(compiled.querySelector('pmo-home')).not.toBe(null);
+    });
+  });
+
+  it('should render the RestaurantsComponent with router navigates to "/restaurants" path', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    router.navigate(['restaurants']).then(() => {
+      expect(location.path()).toBe('/restaurants');
+      expect(compiled.querySelector('pmo-restaurant')).not.toBe(null);
+    });
   });
 });
