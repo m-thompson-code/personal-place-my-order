d6e1e8e7eb5611ef4950db4833476cc638bb0188
Mark Thompson <mark.thompson@smpl.company>
2020-11-13 15:42:10 -0800

    Writing a Restaurant Service: added restaurant service and specs that hits api with get requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { ImageUrlPipe } from './image-url.pipe';
+import { RestaurantService } from './restaurant/restaurant.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,12 @@ import { ImageUrlPipe } from './image-url.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    RestaurantService,// Not needed yet, but wanted to get this out of the way since I assume we'll actually be using this service later
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/restaurant/restaurant.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getRestaurants(): Observable<any> {
+    return this.httpClient.get<any>(`${environment.apiUrl}/restaurants`);
+  }
+}
